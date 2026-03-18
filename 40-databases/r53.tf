0643488ab69c487d44efadb62f87c340a1dbb396
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-18 23:33:35 +0530

    Removed terraform provider files

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -4,6 +4,7 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.MongoDBEC2.private_ip]
+  allow_overwrite = true
 }
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
@@ -11,4 +12,22 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.RedisEC2.private_ip]
+  allow_overwrite = true
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
