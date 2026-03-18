8022090bd62f636d3252af2f2c8778892324e382
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-18 18:25:51 +0530

    Removed terraform provider files

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,1 +1,8 @@
+component=$1
 sudo dnf install ansible -y
+
+
+cd /home/ec2-user
+git clone https://github.com/ravitejakajjamdevops-prog/ansible-roboshop-roles-tf.git
+cd ansible-roboshop-roles-tf
+ansible-playbook -e component=$component roboshop.yaml
