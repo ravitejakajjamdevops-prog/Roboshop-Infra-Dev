847ad53378b854ad3114507e0f6f0eee4b29ee58
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-21 00:02:59 +0530

    Removed terraform provider files

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -121,23 +121,6 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
-resource "aws_launch_template" "foobar" {
-  name_prefix   = "foobar"
-  image_id      = "ami-1a2b3c"
-  instance_type = "t2.micro"
-}
-
-resource "aws_autoscaling_group" "bar" {
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.foobar.id
-    version = "$Latest"
-  }
-}
 resource "aws_autoscaling_policy" "main" {
   autoscaling_group_name = aws_autoscaling_group.main.name
   name                   = "${var.project}-${var.environment}-catalogue"
