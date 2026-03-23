0a680188ab569298ee5032e101e019cc2bb8a9f2
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-23 20:31:52 +0530

    Removed terraform provider files

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component"{
     for_each = var.components
-    source = "::https://github.com/ravitejakajjamdevops-prog/Roboshop-terraform-component.git"
+    source = "git::https://github.com/ravitejakajjamdevops-prog/Roboshop-terraform-component.git"
     component = each.key
     rule_priority = each.value.rule_priority
 
