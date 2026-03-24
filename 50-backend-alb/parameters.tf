e52a40f760d01694f978ed74ce64993157dbd69f
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-24 12:46:19 +0530

    Removed terraform provider files

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -3,4 +3,5 @@ resource "aws_ssm_parameter" "backend-alb_listener_arn" {
   name  = "/${var.project}/${var.environment}/backend-lb_listener_arn"
   type  = "String"
   value = aws_lb_listener.backend-alb.arn
+  overwrite = true
 }
--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -3,5 +3,25 @@ variable "components"{
         catalogue = {
             rule_priority = 10
         }
+
+        user = {
+            rule_priority = 20
+        }
+
+        cart = {
+            rule_priority = 30
+        }
+
+        shipping = {
+            rule_priority = 40
+        }
+
+        payment = {
+            rule_priority = 50
+        }
+        
+        frontend = {
+            rule_priority = 10
+        }
     }
 }
